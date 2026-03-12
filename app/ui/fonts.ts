627dcce202ee25c82f36b895a145c327bad3546b
argo <pegistr@gmail.com>
2026-03-12 14:04:25 +0200

    font fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,11 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
-const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
  
 export const fonts = {
 	inter, lusitana
 };
-
-export inter;
-export lusitana;
